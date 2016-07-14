5bddde26578f245d204ba54d482ff9f8b36d9df9
Allen Kim <akim@cetaris.com>
2016-07-14 17:23:22 -0400

    dist files are added

--- test/systemjs.config.js
+++ test/systemjs.config.js
@@ -17,8 +17,8 @@
 
   map['ng2-scrollable'] = '../src';
   packages['ng2-scrollable'] = {main: 'index.ts', defaultExtension: 'ts'};
-  // map['ng2-scrollable'] = '../dist';
-  // packages['ng2-scrollable'] = {main: 'index.js', defaultExtension: 'js'};
+  map['ng2-scrollable'] = '../dist';
+  packages['ng2-scrollable'] = {main: 'index.js', defaultExtension: 'js'};
 
   System.config({
     transpiler: 'typescript', //use typescript for compilation
